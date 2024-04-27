51125aae6c76fbc65397bea5bc6d7a8dc2319cdd
johannavargas5 <johanna.vargasf@usil.pe>
2024-04-27 18:10:55 -0500

    Johanna

--- Ecommers_Pro/manage.py
+++ Ecommers_Pro/manage.py
@@ -13,10 +13,12 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment? Johanna"
         ) from exc
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+    
+
