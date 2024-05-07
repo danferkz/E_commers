9e6a71e4faf8a1d5e703b0aa10d19c4639d7a986
Alonso Salas <aesc542@outlook.com>
2024-05-07 18:41:10 -0500

    Header component added

---
+++ Ecommers_Pro/ecommersPro/my-project/frontend/frontendEcommers/src/components/Header.vue
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<template>
+        <header class = "GHeader">
+            <div class="logo">
+                <img src="../assets/vue.svg" alt="Logo de la empresa">
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Buscar productos por nombre...">
+                <button type="submit">Buscar</button>
+            </div>  
+            <div class="cart">
+            <button type="button">Carrito</button>
+            </div>
+        </header>
+</template>
+
+<style>
+/* Estilos generales */
+
+.GHeader {
+  background-color: #333;
+  position: fixed;
+  color: #fff;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 100vw
+  
+}
+
+.logo img {
+  height: 50px; /* Ajustar el tamaño del logo según sea necesario */
+}
+
+.search input[type="text"] {
+  padding: 8px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  margin-right: 5px;
+}
+
+.search button {
+  padding: 8px 15px;
+  border-radius: 5px;
+  border: none;
+  background-color: #4CAF50;
+  color: white;
+  cursor: pointer;
+}
+
+.cart button {
+  padding: 8px 15px;
+  border-radius: 5px;
+  border: none;
+  background-color: #008CBA;
+  color: white;
+  cursor: pointer;
+}
+</style>
