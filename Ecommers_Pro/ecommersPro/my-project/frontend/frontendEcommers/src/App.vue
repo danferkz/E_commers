58dbbd65de285c8942035ffb5c05eba7289b8939
Alonso Salas <aesc542@outlook.com>
2024-05-18 17:18:49 -0700

    Added Categories.vue and fixed Carrousel.vue

--- Ecommers_Pro/ecommersPro/my-project/frontend/frontendEcommers/src/App.vue
+++ Ecommers_Pro/ecommersPro/my-project/frontend/frontendEcommers/src/App.vue
@@ -2,11 +2,13 @@
 import WebHeader from './components/Header.vue'
 import WebFooter from './components/Footer.vue'
 import WebCarrousel from './components/Carrousel.vue'
+import WebCategories from './components/Categories.vue'
 </script>
 
 <template>
   <WebHeader/>
-  <WebCarrousel/>   
+  <WebCarrousel/>
+  <WebCategories/>   
   <WebFooter/>
 </template>
 
--- Ecommers_Pro/ecommersPro/my-project/frontend/frontendEcommers/src/components/Carrousel.vue
+++ Ecommers_Pro/ecommersPro/my-project/frontend/frontendEcommers/src/components/Carrousel.vue
@@ -1,64 +1,91 @@
 <template>
-    <div class="carousel-container">
-      <div class="carousel" ref="carousel">
-        <img v-for="(image, index) in images" :src="image" :key="index" alt="Carousel Image" class="carousel-image">
-        <img >
+  <div class="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item" v-for="(item, index) in items" :key="index">
+        <img :src="item.src" :alt="item.alt">
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        images: [
-        "../sets/vue.svg",
-        "../assets/carru.jpg",
-        "../assets/carru.jpg"
-        ],
-        currentIndex: 0,
-        intervalId: null
-      };
+    <a class="carousel-control-prev" @click="prevSlide">&#10094;</a>
+    <a class="carousel-control-next" @click="nextSlide">&#10095;</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { src: 'https://via.placeholder.com/800x400?text=Slide+1', alt: 'Slide 1' },
+        { src: 'https://via.placeholder.com/800x400?text=Slide+2', alt: 'Slide 2' },
+        { src: 'https://via.placeholder.com/800x400?text=Slide+3', alt: 'Slide 3' }
+      ],
+      currentIndex: 0
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+      this.updateSlide();
     },
-    mounted() {
-      this.startCarousel();
+    prevSlide() {
+      this.currentIndex = (this.currentIndex - 1 + this.items.length) % this.items.length;
+      this.updateSlide();
     },
-    methods: {
-      startCarousel() {
-        this.intervalId = setInterval(() => {
-          this.currentIndex = (this.currentIndex + 1) % this.images.length;
-          this.scrollCarousel();
-        }, 3000);
-      },
-      scrollCarousel() {
-        const carousel = this.$refs.carousel;
-        carousel.scrollTo({
-          left: carousel.offsetWidth * this.currentIndex,
-          behavior: "smooth"
-        });
-      },
-      stopCarousel() {
-        clearInterval(this.intervalId);
-      }
-    },
-    beforeDestroy() {
-      this.stopCarousel();
+    updateSlide() {
+      const carouselInner = this.$el.querySelector('.carousel-inner');
+      const width = this.$el.clientWidth;
+      carouselInner.style.transform = `translateX(-${this.currentIndex * width}px)`;
     }
-  };
-  </script>
-  
-  <style scoped>
-  .carousel-container {
-    overflow: hidden;
+  },
+  mounted() {
+    this.updateSlide();
+    window.addEventListener('resize', this.updateSlide);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateSlide);
   }
-  
-  .carousel {
-    display: flex;
-  }
-  
-  .carousel-image {
-    width: 40%;
-    flex-shrink: 0;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.carousel {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.carousel-inner {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+}
+
+.carousel-item {
+  min-width: 100%;
+  box-sizing: border-box;
+}
+
+.carousel-item img {
+  width: 100%;
+  display: block;
+}
+
+.carousel-control-prev,
+.carousel-control-next {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  user-select: none;
+}
+
+.carousel-control-prev {
+  left: 10px;
+}
+
+.carousel-control-next {
+  right: 10px;
+}
+</style>
