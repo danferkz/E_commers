39722a1e5b1866131e3a83c5593cb91571031805
danfer <gdanfer07@gmail.com>
2024-05-24 13:31:53 -0500

    media

--- ecommerceMayorazgo_/ecommerceMayorazgo/settings/base.py
+++ ecommerceMayorazgo_/ecommerceMayorazgo/settings/base.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +138,8 @@ AUTH_USER_MODEL = 'user.Users'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
