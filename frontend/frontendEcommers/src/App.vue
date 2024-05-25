06529940feaad5346c69183e41daad642e88cd56
danfer <gdanfer07@gmail.com>
2024-05-25 11:25:25 -0500

    node_descargas_2

--- frontend/frontendEcommers/src/App.vue
+++ frontend/frontendEcommers/src/App.vue
@@ -6,29 +6,11 @@ import WebCategories from './components/Categories.vue'
 </script>
 
 <template>
-  <template>
-  <div class="container">
-    <!-- NAVBAR -->
-    <navbar-component-vue v-if="$route.path != '/'" />
-
-    <!-- NAVIGATION -->
-    <navigation-component-vue v-if="$route.path === '/about'" />
-
-    <router-view/>
-
-    <!-- FOOTER -->
-    <footer-component-vue />
-  </div>
-
-</template>
-<script setup>
-  import NavbarComponentVue from './components/NavbarComponent.vue'
-  import NavigationComponentVue from './components/NavigationComponent.vue'
-  import FooterComponentVue from './components/FooterComponent.vue'
-</script>
-
+  <WebHeader/>
+  <WebCarrousel/>
+  <WebCategories/>   
+  <WebFooter/>
 </template>
 
 <style>
-
 </style>
--- frontend/frontendEcommers/src/main.js
+++ frontend/frontendEcommers/src/main.js
@@ -1,13 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router';
-import store from './store';
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-console.log(import.meta.env.VITE_API_URL); // Output: 'http://localhost:5173'
-console.log(import.meta.env.VITE_DEBUG_MODE);
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).mount('#app')
